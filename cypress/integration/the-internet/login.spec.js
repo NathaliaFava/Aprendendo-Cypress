2ecf949f9b14d8cdfa4f02a36c25fcff379fc46c
NathaliaFava <natfava2017@gmail.com>
2020-07-01 21:22:21 -0300

    Iniciando o projeto com cypress

---
+++ cypress/integration/the-internet/login.spec.js
@@ -0,0 +1,32 @@
+/// < reference types="cypress" />
+
+import elements from '../../support/login'
+
+
+describe('Form Authentication utilizando o The Internet"', () => {
+
+    beforeEach(() => {
+        cy.visit('https://the-internet.herokuapp.com/login')
+    })   
+
+    it('Deve visualizar mensagem de sucesso ao informar os dados corretos', () => {
+         cy.get(elements.login.username).type('tomsmith')
+         cy.get(elements.login.password).type('SuperSecretPassword!')
+         cy.get(elements.login.btn_Login).click()
+         cy.get(elements.login.message).should('contain.text', 'You logged into a secure area!')
+    })
+
+    it('Deve visualizar mensagem de erro ao informar nome incorreto', () => {
+        cy.get(elements.login.username).type('TomSmith')
+        cy.get(elements.login.password).type('SuperSecretPassword!')
+        cy.get(elements.login.btn_Login).click()
+        cy.get(elements.login.message).should('contain.text', 'Your username is invalid!')
+   })
+
+   it('Deve visualizar mensagem de erro ao informar senha incorreta', () => {
+        cy.get(elements.login.username).type('tomsmith')
+        cy.get(elements.login.password).type('#SuperSecretPassword#')
+        cy.get(elements.login.btn_Login).click()
+        cy.get(elements.login.message).should('contain.text', 'Your password is invalid!')
+})
+})
---
+++ cypress/support/login.js
@@ -0,0 +1,10 @@
+const login = {
+    login: {
+        username: '#username',
+        password: '#password',
+        btn_Login: '.radius',
+        message: '#flash'
+    }
+}
+
+export default login
