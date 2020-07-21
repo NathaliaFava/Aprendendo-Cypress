2226d476a5235dbc4e359e1a044016a1f84db70e
NathaliaFava <natfava2017@gmail.com>
2020-07-21 20:00:16 -0300

    Alterando o contains para should e atualização do package

--- cypress/integration/the-internet/login.spec.js
+++ cypress/integration/the-internet/login.spec.js
@@ -13,20 +13,20 @@ describe('Form Authentication utilizando o The Internet"', () => {
          cy.get(elements.login.username).type('tomsmith')
          cy.get(elements.login.password).type('SuperSecretPassword!')
          cy.get(elements.login.btn_Login).click()
-         cy.get(elements.login.message).contains('You logged into a secure area!')
+         cy.get(elements.login.message).should('contain','You logged into a secure area!')
     })
 
     it('Deve visualizar mensagem de erro ao informar nome incorreto', () => {
         cy.get(elements.login.username).type('TomSmith')
         cy.get(elements.login.password).type('SuperSecretPassword!')
         cy.get(elements.login.btn_Login).click()
-        cy.get(elements.login.message).contains('Your username is invalid!')
+        cy.get(elements.login.message).should('contain','Your username is invalid!')
    })
 
    it('Deve visualizar mensagem de erro ao informar senha incorreta', () => {
         cy.get(elements.login.username).type('tomsmith')
         cy.get(elements.login.password).type('#SuperSecretPassword#')
         cy.get(elements.login.btn_Login).click()
-        cy.get(elements.login.message).contains('Your password is invalid!')
+        cy.get(elements.login.message).should('contain','Your password is invalid!')
 })
 })
